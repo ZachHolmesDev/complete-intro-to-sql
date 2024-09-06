654d0e5f424a30a473828d9d11a9c333f9dfbdd4
Zach Holmes <zholmes430@gmail.com>
2024-09-06 21:41:15 +1000

    add sql file

--- practice/index.js
+++ practice/index.js
@@ -1,1 +1,13 @@
 const pg = require("pg");
+const pool = new pg.Pool({
+	user: "postgres",
+	host: "localhost",
+	database: "recipeguru	",
+	password: "lol",
+	port: 5432,
+});
+
+const { rows } = await pool.query(`SELECT * FROM recepies`);
+
+
+console.log(rows)
