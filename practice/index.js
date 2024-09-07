cfd7a85e7f657c15da50dab0a10096350dafbc88
Zach Holmes <zholmes430@gmail.com>
2024-09-07 12:13:28 +1000

    add submodule

--- practice/index.js
+++ practice/index.js
@@ -1,13 +1,15 @@
-const pg = require("pg");
-const pool = new pg.Pool({
-	user: "postgres",
-	host: "localhost",
-	database: "recipeguru	",
-	password: "lol",
-	port: 5432,
+import pg from "pg"; // Use import instead of require
+const { Pool } = pg; // Destructure the Pool from pg
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "recipeguru",
+  password: "lol",
+  port: 5432,
 });
 
-const { rows } = await pool.query(`SELECT * FROM recepies`);
+const { rows } = await pool.query(`SELECT * FROM recipes`);
 
 
 console.log(rows)
